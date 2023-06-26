76aa31d6176bb28636ea3bf3bc7ab6f1843dc3d8
thomas-nicholson <tnicholson128@protonmail.com>
2023-06-26 07:58:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let countdownDate = new Date("2023-08-08T00:00:00").getTime();
+
+let x = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countdownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days + "d";
+  document.getElementById("hours").innerHTML = hours + "h";
+  document.getElementById("minutes").innerHTML = minutes + "m";
+  document.getElementById("seconds").innerHTML = seconds + "s";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "";
+    document.getElementById("hours").innerHTML = "";
+    document.getElementById("minutes").innerHTML = "";
+    document.getElementById("seconds").innerHTML = "TIME'S UP";
+  }
+}, 1000);
